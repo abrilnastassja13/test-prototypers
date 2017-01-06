9d13e10d8e18339bff378f3f7b4c9d903e0c209a
abrilnastassja13 <abril.saravia13@gmail.com>
2017-01-06 11:05:58 -0300

    secciones listas

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+$(window).load(function() {
+	$(".btn-nav").on("click tap", function() {
+    $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+
+    $(this).toggleClass("animated");
+
+  });
+});
+
+});
+
+
